097e984ec8ff7c919763bfd4083f1117864068e1
onikun94 <onikun94@gmail.com>
2022-11-21 19:53:55 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { type NextPage } from "next";
+import { Text } from "@chakra-ui/react"
+
+const Home:NextPage = () => {
+  return (
+    <>
+        <Text fontSize="3xl" backgroundColor="cyan">Welcome to Chakra UI</Text>
+    </>
+  )
+}
+
+export default Home
+
+
