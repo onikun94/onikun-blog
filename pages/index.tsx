56b6f5e32f6e4c8b68fe2d2c1210f026600b7093
onikun94 <onikun94@gmail.com>
2022-11-26 23:33:34 +0900

    basic notion blog

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,63 @@
-import { type NextPage } from "next";
-import { Text } from "@chakra-ui/react"
+import { GetStaticProps, type NextPage } from "next";
+import {
+  Badge,
+  Card,
+  CardBody,
+  Container,
+  Flex,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { getNotionData } from "../lib/getNotionData";
+import Link from "next/link";
+import { PageObjectResponse, PartialPageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
-const Home:NextPage = () => {
+type Props = {posts:(PageObjectResponse | PartialPageObjectResponse)[]}
+
+const Home: NextPage<Props> = ({posts}) => {
+  // posts.map((post)=>{
+  //}
+  //const resultText = post.properties.Slug.rich_text[0].plain_text;
+  console.log("typeof = ",typeof posts[0].object)
+  //console.log("POSTS = ", posts);
   return (
-    <>
-        <Text fontSize="3xl" backgroundColor="cyan">Welcome to Chakra UI</Text>
-    </>
-  )
-}
+    <Container marginY="16">
+      <Text fontSize="3xl" backgroundColor="white" marginY="4">
+        All
+      </Text>
+      {posts.map((post) => (
+        <Link href={`/${post.properties.Slug.rich_text[0].plain_text}`}>
+          <Card key={post.id} marginY="2">
+            <CardBody>
+              <Text fontSize="lg" fontWeight="bold">
+                {post.properties.Slug.rich_text[0].plain_text}
+              </Text>
+              <Stack direction="row">
+                {post.properties.Tag.multi_select.map((tag) => (
+                  <Badge key={tag.name} colorScheme={tag.color}>
+                    {tag.name}
+                  </Badge>
+                ))}
+              </Stack>
+              <Text>{post.properties.Description.rich_text[0].plain_text}</Text>
+            </CardBody>
+          </Card>
+        </Link>
+      ))}
+    </Container>
+  );
+  //})
+};
 
-export default Home
+export const getStaticProps:GetStaticProps<Props>  = async () => {
+  const database = await getNotionData(process.env.NOTION_DATABASE_ID);
+  console.log("database = ", database);
 
+  return {
+    props: {
+      posts: database,
+    },
+  };
+};
 
+export default Home;
