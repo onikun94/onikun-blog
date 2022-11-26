fb7dfb1929a4dddd0d16fb13850337883dd9393d
onikun94 <onikun94@gmail.com>
2022-11-26 23:41:06 +0900

    basic detail blog

---
+++ pages/[slug].tsx
@@ -0,0 +1,74 @@
+import { PageObjectResponse, PartialPageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+import { GetStaticProps, GetStaticPropsContext } from "next";
+import React from "react";
+import { RenderBlocks } from "../components/ContentBlocks";
+import { getNotionData, getPage, getBlocks } from "../lib/getNotionData";
+
+//type Props = {posts:(PageObjectResponse | PartialPageObjectResponse)[]}
+type Props = {page:PageObjectResponse,blocks:any[]}
+type Params = {context:string}
+
+const databaseId = process.env.NOTION_DATABASE_ID;
+
+export default function Post ({page, blocks}) {
+  return (
+    <div>
+  <h1 className="mb-5 text-3xl font-bold tracking-tight text-black md:text-5xl">
+        {page.properties.Post.title[0].plain_text}
+      </h1>
+
+      <RenderBlocks blocks={blocks} />
+    </div>
+  );
+};
+
+export const getStaticPaths = async () => {
+  const database = await getNotionData(databaseId);
+  return {
+    paths: database.map((page) => ({
+      params: {
+        slug: page.properties.Slug.rich_text[0].plain_text,
+      },
+    })),
+    fallback: false,
+  };
+};
+export const getStaticProps:GetStaticProps = async (context:GetStaticPropsContext) => {
+  const { slug } = context.params;
+  console.log("context = ",context)
+  const database = await getNotionData(databaseId);
+  console.log("database = ",database)
+  const filter = database.filter(
+    (blog) => blog.properties.Slug.rich_text[0].plain_text === slug
+  );
+  const page = await getPage(filter[0].id);
+  const blocks = await getBlocks(filter[0].id);
+
+  const childrenBlocks = await Promise.all(
+    blocks
+      .filter((block) => block.has_children)
+      .map(async (block) => {
+        return {
+          id: block.id,
+          children: await getBlocks(block.id),
+        };
+      })
+  );
+
+  const blocksWithChildren = blocks.map((block) => {
+    if (block.has_children) {
+      block[block.type].children = childrenBlocks.find(
+        (x) => x.id === block.id
+      ).children;
+    }
+    return block;
+  });
+
+  return {
+    props: {
+      page,
+      blocks: blocksWithChildren,
+    },
+  };
+};
+
